d8fa0f1f61dbde1bde1bc9e9850a398f1901b081
Oluwajoba <oluwajobabello@gmail.com>
2018-05-09 05:32:14 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  getDayName(number) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[number];
+  }
+  getMonthName(number) {
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return months[number];
+  }
+  getYear() {
+    let today2 = new Date().getFullYear();
+    return today2;
+  }
   render() {
-    return (
-      <div>
-       Hello World
-      </div>
-    );
+    // Get current date
+    let today = new Date().getDay();
+    let today1 = new Date().getMonth();
+    
+    // If it is weekend
+    if (today === 6 || today === 0 || today1 === 6 || today1 === 0)
+    return <div>Today is {this.getDayName(today)} {this.getMonthName(today1)}, {this.getYear()} - You can watch TV all day today</div>;
+    // If it is weekdays
+    return <div>Today is {this.getDayName(today)} {this.getMonthName(today1)}, {this.getYear()} - You should go to work today</div>;
   }
 }
 
