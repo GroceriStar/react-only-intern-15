c355669a9a6069cbb440ca2356147d83311f5bf5
Oluwajoba <oluwajobabello@gmail.com>
2018-05-11 21:44:53 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
+import DayChecker from './DayChecker';
+import Header from './Header';
+import Calendar from 'react-calendar'
 
 class App extends Component {
-  getDayName(number) {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return days[number];
-  }
-  getMonthName(number) {
-    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    return months[number];
-  }
-  getYear() {
-    let today2 = new Date().getFullYear();
-    return today2;
-  }
   render() {
-    // Get current date
-    let today = new Date().getDay();
-    let today1 = new Date().getMonth();
-    
-    // If it is weekend
-    if (today === 6 || today === 0 || today1 === 6 || today1 === 0)
-    return <div>Today is {this.getDayName(today)} {this.getMonthName(today1)}, {this.getYear()} - You can watch TV all day today</div>;
-    // If it is weekdays
-    return <div>Today is {this.getDayName(today)} {this.getMonthName(today1)}, {this.getYear()} - You should go to work today</div>;
+    return [
+      <Header />,
+      <Calendar />, 
+      <br />,
+      <DayChecker />
+      
+    ];
   }
 }
-
 export default App;
